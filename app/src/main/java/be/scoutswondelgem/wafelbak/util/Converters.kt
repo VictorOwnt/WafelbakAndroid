04928a5bc8fadaef81c68a0d7aec03d0b60e6619
Victor Van hulle <victor.vanhulle.w3296@student.hogent.be>
2019-12-09 21:43:26 +0100

    Room Converters

--- app/src/main/java/be/scoutswondelgem/wafelbak/util/Converters.kt
+++ app/src/main/java/be/scoutswondelgem/wafelbak/util/Converters.kt
@@ -1,20 +1,52 @@
 package be.scoutswondelgem.wafelbak.util
 
 import androidx.room.*
+import be.scoutswondelgem.wafelbak.database.dataAccessObjects.OrderDao
+import be.scoutswondelgem.wafelbak.database.entities.DBOrder
+import java.text.ParseException
+import java.text.SimpleDateFormat
 import java.util.*
-
-
-
+import javax.inject.Inject
 
 class Converters {
+    @Inject
+    internal lateinit var orderDao: OrderDao
+    private var format = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.sss'Z'", Locale.getDefault())
+
     @TypeConverter
-    fun toDate(dateLong:Long): Date {
-        return Date(dateLong)
+    fun stringtoDate(stringDate: String): Date? {
+        return try {
+            format.parse(stringDate)
+        }
+        catch (e: ParseException) {
+            null
+        }
     }
 
     @TypeConverter
-    fun fromDate(date: Date):Long{
-        return date.time
+    fun dateToString(date: Date):String{
+        return format.format(date)
     }
 
+    @TypeConverter
+    fun ordersToString(orders: List<DBOrder>): String {
+        var output = ""
+        orders.forEach{ order ->
+            output.plus(order.id.toString() + ",")
+        }
+        return output
+    }
+
+    @TypeConverter
+    fun stringToOrders(orders: String): List<DBOrder> {
+        var orderList: MutableList<DBOrder> = mutableListOf()
+        var orderIds = orders.split(',')
+        orderIds.forEach{ orderId ->
+            var order = orderDao.getById(orderId.toInt())
+            if (order !== null) {
+                orderList.add(order)
+            }
+        }
+        return orderList
+    }
 }
